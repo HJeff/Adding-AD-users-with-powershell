3fe1475db1c920ca416d6c51d0c9a8381ae6b028
HJeff <JHHsieh1990@gmail.com>
2019-10-12 23:26:58 -0700

    Cleaning_PC script
    Just a little script I used

---
+++ clean_PC.ps1
@@ -0,0 +1,24 @@
+
+#cleaner for PC
+
+
+sleep(5)
+
+write-output "please wait for cleaning to finish";
+
+
+cleanmgr.exe /dC
+
+
+
+
+$user_Input = read_Host -prompt "Would you like to de-frag your computer? (If yes, please run this overnight) Y/N?"
+
+if($user_Input -eq Y -or $user_Input -eq y ){
+	defrag.exe /C
+}
+else{
+	write-out "Okay!"
+	sleep(3)
+	exit
+}
